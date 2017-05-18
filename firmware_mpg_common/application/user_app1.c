5b949fab61f1d9db44a4e338d69ac539ca2e635f
ShiChenjunJ <491883744@qq.com>
2017-05-18 21:06:32 +0800

    The homework of LCD

--- firmware_mpg_common/application/user_app1.c
+++ firmware_mpg_common/application/user_app1.c
@@ -52,7 +52,7 @@ extern volatile u32 G_u32ApplicationFlags;             /* From main.c */
 extern volatile u32 G_u32SystemTime1ms;                /* From board-specific source file */
 extern volatile u32 G_u32SystemTime1s;                 /* From board-specific source file */
 
-
+extern u8 G_au8DebugScanfBuffer[];
 /***********************************************************************************************************************
 Global variable definitions with scope limited to this local application.
 Variable names shall start with "UserApp1_" and be declared as static.
@@ -87,10 +87,23 @@ Promises:
 */
 void UserApp1Initialize(void)
 {
+  
  
+  LedOff(WHITE);
+  LedOff(PURPLE);
+  LedOff(BLUE); 
+  LedOff(CYAN);
+  LedOff(GREEN);      
+  LedOff(YELLOW);
+  LedOff(ORANGE);       
+  LedOff(RED);
+  
+  
   /* If good initialization, set state to Idle */
   if( 1 )
   {
+    
+    LCDCommand(LCD_CLEAR_CMD);
     UserApp1_StateMachine = UserApp1SM_Idle;
   }
   else
@@ -128,6 +141,87 @@ void UserApp1RunActiveState(void)
 /*--------------------------------------------------------------------------------------------------------------------*/
 
 
+/*Move the String2 and spme part of String3.One bit One Time*/
+static void MoveLeft(u8 au8String2[],u8 au8String3[],u8 u8Lenghtof3)
+{
+  u8 u8Counter2;
+  u8 u8First1;
+  u8 u8First2;
+  u8First1=au8String2[0];
+  u8First2=au8String3[0];
+  
+  
+  for(u8Counter2=0;u8Counter2<39;u8Counter2++)
+    {
+      au8String2[u8Counter2]=au8String2[u8Counter2+1];
+    }
+  
+  au8String2[39]=u8First2;
+  
+  for(u8Counter2=0;u8Counter2<(u8Lenghtof3-1);u8Counter2++)
+  {
+    au8String3[u8Counter2]=au8String3[u8Counter2+1];
+  }
+  
+  au8String3[u8Lenghtof3-1]=u8First1;
+  
+}
+
+/*To counter how many bits we input*/
+static u8 COUNTER(u8 au8String4[])
+{
+  u8 u8Counter3=0;
+  
+  for(;u8Counter3<255;u8Counter3++)
+  {
+    if(au8String4[u8Counter3]=='\r')
+    {
+      break;
+    }
+  }
+   
+  return u8Counter3;
+}
+
+/*To know the inputing end or not*/
+static bool TheInputEnd(u8 au8String5[])
+{
+  u8 u8Counter4=0;
+  bool bEnd=FALSE;
+  
+  for(;u8Counter4<255;u8Counter4++)
+  {
+    if(au8String5[u8Counter4]=='\r')
+    {
+      bEnd=TRUE;
+      break;
+    }
+  }
+  
+  return bEnd;
+}
+
+/*The top 20 of au8Words is given to Line1*/
+static void LINE1(u8 au8Line1[],u8 au8Words[])
+{
+  u8 u8Counter01;
+  for(u8Counter01=0;u8Counter01<20;u8Counter01++)
+  {
+    au8Line1[u8Counter01]=au8Words[u8Counter01];
+  }
+}
+
+/*The last 20 of au8Words is given to Line2*/
+static void LINE2(u8 au8Line1[],u8 au8Words[])
+{
+  u8 u8Counter02;
+  for(u8Counter02=0;u8Counter02<20;u8Counter02++)
+  {
+    au8Line1[u8Counter02]=au8Words[u8Counter02+20];
+  }
+}
+     
+           
 /**********************************************************************************************************************
 State Machine Function Definitions
 **********************************************************************************************************************/
@@ -136,7 +230,228 @@ State Machine Function Definitions
 /* Wait for ??? */
 static void UserApp1SM_Idle(void)
 {
+  static u8 u8Led=0;
+  static u8 u8Counter1=0;
+  static u8 au8ReadWords[255]="\0";
+  static u8 au8Move[40]="                                        ";
+  static u8 u8OneTime=1;
+  static u8 au8DisplayName[20]="***Shi**Chen**Jun***";
+  static u8 au8Line1[20]="\0";
+  static u8 au8Line2[20]="\0";
+  static u32 u32Time1=0;
+  static u32 u32Time2=0;
+  static u32 u32Time3=0;
+  static u32 u32Time4=0;
+  static bool bMove1=FALSE;
+  static bool bMove2=FALSE; 
+  static bool bInput=FALSE;
+  static bool bLed=TRUE;
+/*To check the inputing.If it is ended.Turn off the diaplaying of name and go to display the 
+  words inputed*/  
+  if(!bInput)
+  {
+  strcpy(au8ReadWords,G_au8DebugScanfBuffer);
+  }
+  
+  bInput=TheInputEnd(au8ReadWords);
+/*if there is not inputing.Wait and display again and again*/
+if(!bInput)
+{
+  u32Time1++;
+  
+  if(u32Time1==500)
+  {
+    bMove1=TRUE;
+    u32Time1=0;
+  }
+  else
+  {
+    bMove1=FALSE;
+  }  
+  
+  if(bMove1)
+  {
+    
+    LINE1(au8Line1,au8Move);
+    LINE2(au8Line2,au8Move);  
+    LCDMessage(LINE1_START_ADDR,au8Line1);
+    LCDMessage(LINE2_START_ADDR,au8Line2);
+    MoveLeft(au8Move,au8DisplayName,20);
+  }
+}
+/*Display the words*/
+else
+{
+  u8Counter1=COUNTER(au8ReadWords);
+  if(u8Counter1<=41)
+  {
+    LCDCommand(LCD_CLEAR_CMD);
+    LINE1(au8Line1,au8ReadWords);
+    LCDMessage(LINE1_START_ADDR,au8Line1);
+    LINE2(au8Line2,au8ReadWords);
+    LCDMessage(LINE2_START_ADDR,au8Line2);
+  }
+  else
+  {
+    u32Time2++;
+  
+    if(u32Time2==500)
+    {
+      bMove2=TRUE;
+      u32Time2=0;
+    }
+    else
+    {
+      bMove2=FALSE;
+    }
+     
+    if(bMove2)
+    {
+      if(u8OneTime)
+      {
+        u8 u8CON=0;
+        for(u8CON=0;u8CON<40;u8CON++)
+        {
+          au8Move[u8CON]=' ';
+        }
+        u8OneTime=0;
+      }
+      
+      LINE1(au8Line1,au8Move);
+      LINE2(au8Line2,au8Move);  
+      LCDMessage(LINE1_START_ADDR,au8Line1);
+      LCDMessage(LINE2_START_ADDR,au8Line2);
+      MoveLeft(au8Move,au8ReadWords,u8Counter1);
+    }
+    
+  }
+}
+  
+/*Change the colour of LED every 500ms.THe BUTTON0 can turn off the */
+if(bLed)
+ { 
+  u32Time3++;
+  
+  if(u32Time3==500)
+  {
+     switch(u8Led)
+       {
+        case 0:LedOff(WHITE);
+               LedOff(PURPLE);
+               LedOff(BLUE); 
+               LedOff(CYAN);
+               LedOff(GREEN);      
+               LedOff(YELLOW);
+               LedOff(ORANGE);       
+               LedOn(RED);break;  
+    
+        case 1:LedOff(WHITE);
+               LedOff(PURPLE);
+               LedOff(BLUE); 
+               LedOff(CYAN);
+               LedOff(GREEN);      
+               LedOff(YELLOW);
+               LedOn(ORANGE);       
+               LedOff(RED);break;   
+      
+        case 2:LedOff(WHITE);
+               LedOff(PURPLE);
+               LedOff(BLUE); 
+               LedOff(CYAN);
+               LedOff(GREEN);      
+               LedOn(YELLOW);
+               LedOff(ORANGE);       
+               LedOff(RED);break;   
+      
+        case 3:LedOff(WHITE);
+               LedOff(PURPLE);
+               LedOff(BLUE); 
+               LedOff(CYAN);
+               LedOn(GREEN);      
+               LedOff(YELLOW);
+               LedOff(ORANGE);       
+               LedOff(RED);break;  
+      
+        case 4:LedOff(WHITE);
+               LedOff(PURPLE);
+               LedOff(BLUE); 
+               LedOn(CYAN);
+               LedOff(GREEN);      
+               LedOff(YELLOW);
+               LedOff(ORANGE);       
+               LedOff(RED);break;  
+      
+        case 5:LedOff(WHITE);
+               LedOff(PURPLE);
+               LedOn(BLUE); 
+               LedOff(CYAN);
+               LedOff(GREEN);      
+               LedOff(YELLOW);
+               LedOff(ORANGE);       
+               LedOff(RED);break; 
+           
+        case 6:LedOff(WHITE);
+               LedOn(PURPLE);
+               LedOff(BLUE); 
+               LedOff(CYAN);
+               LedOff(GREEN);      
+               LedOff(YELLOW);
+               LedOff(ORANGE);       
+               LedOff(RED);break;         
+           
+        case 7:LedOn(WHITE);
+               LedOff(PURPLE);
+               LedOff(BLUE); 
+               LedOff(CYAN);
+               LedOff(GREEN);      
+               LedOff(YELLOW);
+               LedOff(ORANGE);       
+               LedOff(RED);break;          
+           
+        default:break;       
+       }
+        u8Led++;
+        u32Time3=0;
+        
+       if(u8Led==8)
+       {
+        u8Led=0;
+       }
+   }
+ }
 
+  if(WasButtonPressed(BUTTON0))
+  {
+   ButtonAcknowledge(BUTTON0);
+   u8Led=0;
+   u32Time3=0;
+   bLed=!bLed;
+   LedOff(WHITE);
+   LedOff(PURPLE);
+   LedOff(BLUE); 
+   LedOff(CYAN);
+   LedOff(GREEN);      
+   LedOff(YELLOW);
+   LedOff(ORANGE);       
+   LedOff(RED);
+  } 
+
+/*Buzzer 2s*/  
+if(1)
+  {
+   if(u32Time4<2000)
+   {
+   u32Time4++;
+   ButtonAcknowledge(BUTTON1);
+   PWMAudioOn(BUZZER1);
+   PWMAudioSetFrequency(BUZZER1, 294);
+   }
+   
+   else
+   {
+     PWMAudioOff(BUZZER1);
+   }
+  }
 } /* end UserApp1SM_Idle() */
     
 #if 0
